794beb67e0436e62b93ff439dcf5bc401bdf2e76
Dilip Maurya <143182090+dilipMaurya1586@users.noreply.github.com>
2026-07-25 09:24:04 +0530

    Time: 3 ms (86.52%), Space: 47.8 MB (32.11%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int candy(int[] ratings) {
+
+        int n = ratings.length;
+        int[] candies = new int[n];
+
+        //base case
+        for (int i = 0; i < n; i++) {
+            candies[i] = 1;
+        }
+
+        //left
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        //right
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        // sune of all candies
+        int total = 0;
+        for(int c : candies) {
+            total += c;
+        }
+        return total;
+    }
+}
