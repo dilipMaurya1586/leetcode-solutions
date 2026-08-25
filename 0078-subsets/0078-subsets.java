c62a37ad28871a799c553805565df31e03a85f9e
Dilip Maurya <143182090+dilipMaurya1586@users.noreply.github.com>
2026-08-25 12:40:46 +0530

    Time: 1 ms (86.56%), Space: 44 MB (75.62%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public List<List<Integer>> subsets(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        backtrack(0, nums, new ArrayList<>(), result);
+
+        return result;
+    }
+
+    private void backtrack(
+            int index,
+            int[] nums,
+            List<Integer> current,
+            List<List<Integer>> result) {
+
+        // Every current combination is a valid subset
+        result.add(new ArrayList<>(current));
+
+        for (int i = index; i < nums.length; i++) {
+
+            // Take nums[i]
+            current.add(nums[i]);
+
+            // Move forward
+            backtrack(i + 1, nums, current, result);
+
+            // Undo the choice
+            current.remove(current.size() - 1);
+        }
+    }
+}
