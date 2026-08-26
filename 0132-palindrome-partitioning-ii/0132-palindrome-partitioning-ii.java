f39010e99c16ecbb511ec309c4bdf2a3616d0308
Dilip Maurya <143182090+dilipMaurya1586@users.noreply.github.com>
2026-08-26 18:05:55 +0530

    Time: 30 ms (73.98%), Space: 47.8 MB (21.54%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minCut(String s) {
+
+        int n = s.length();
+        boolean[][] isPal = new boolean[n][n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j) && (j - i <= 2 || isPal[i + 1][j - 1])) {
+                    isPal[i][j] = true;
+                }
+            }
+        }
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (isPal[0][i]) {
+                dp[i] = 0;
+            } else {
+                dp[i] = i;
+                for (int j = 1; j <= i; j++) {
+                    if (isPal[j][i]) {
+                        dp[i] = Math.min(dp[i], dp[j - 1] + 1);
+                    }
+                }
+            }
+        }
+        return dp[n - 1];
+    }
+}
