6538d5dec0fd26c34633a6fcbc9594929f04ea02
Dilip Maurya <143182090+dilipMaurya1586@users.noreply.github.com>
2026-09-07 22:29:50 +0530

    Time: 6 ms (76.86%), Space: 47.1 MB (50.14%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    Map<String, PriorityQueue<String>> graph = new HashMap<>();
+    LinkedList<String> result = new LinkedList<>();
+
+    public List<String> findItinerary(List<List<String>> tickets) {
+
+        // Build graph
+        for (List<String> ticket : tickets) {
+
+            String from = ticket.get(0);
+            String to = ticket.get(1);
+            graph.computeIfAbsent(from, k -> new PriorityQueue<>()).offer(to);
+        }
+        dfs("JFK");
+        return result;
+    }
+
+    private void dfs(String airport) {
+
+        PriorityQueue<String> destinations = graph.get(airport);
+
+        while (destinations != null && !destinations.isEmpty()) {
+
+            String next = destinations.poll();
+            dfs(next);
+        }
+        result.addFirst(airport);
+    }
+}
